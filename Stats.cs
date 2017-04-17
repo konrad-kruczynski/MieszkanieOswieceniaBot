507b6ea593ed875b9412127b1c4715cb0db8516c
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2017-04-17 15:30:58 +0200

    Stats improved a little bit.

--- Stats.cs
+++ Stats.cs
@@ -13,10 +13,11 @@ namespace MieszkanieOswieceniaBot
         public string GetStats()
         {
             var builder = new StringBuilder();
-            builder.AppendFormat("Jestem uruchomiony od {0:D}, czyli przez {1:#.#}.", startTime,
-                                 (DateTime.Now - startTime).TotalDays);
+            var totalDays = (DateTime.Now - startTime).TotalDays;
+            builder.AppendFormat("Jestem uruchomiony od {0:D}, czyli przez {1:0.0} dnia.", startTime, totalDays);
             builder.AppendLine();
-            builder.AppendFormat($"Obsłużyłem w tym czasie {messageCounter} wiadomości.");
+            builder.AppendLine($"Obsłużyłem w tym czasie {messageCounter} wiadomości.");
+            builder.AppendFormat("To daje średnio ~{0:0.0} wiadomości dziennie.", messageCounter / totalDays);
             return builder.ToString();
         }
 
