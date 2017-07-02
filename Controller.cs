0c9d96f4d5b0562296a9b4299c293121c4eca242
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2017-07-02 17:19:10 +0200

    Forgotten using.

--- Controller.cs
+++ Controller.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Reactive.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 
 namespace MieszkanieOswieceniaBot
