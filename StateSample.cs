8aaf66d358e595f45fc3c768b5f7ae02cc8f7d4c
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2018-02-13 17:43:21 +0100

    Forgotten condition.

--- StateSample.cs
+++ StateSample.cs
@@ -24,7 +24,7 @@ namespace MieszkanieOswieceniaBot
         {
             get
             {
-                return !GetStateArray().Any();
+                return !GetStateArray().Any(x => x);
             }
         }
 
