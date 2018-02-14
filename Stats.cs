078274c7235dea4b7ecc485988fcd163367a71bb
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2018-02-14 19:21:54 +0100

    Better time since start message.

--- Stats.cs
+++ Stats.cs
@@ -15,11 +15,11 @@ namespace MieszkanieOswieceniaBot
         public string GetStats()
         {
             var builder = new StringBuilder();
-            var totalDays = (DateTime.Now - startTime).TotalDays;
-            builder.AppendFormat("Jestem uruchomiony od {0:D}, czyli przez {1:0.0} dnia.", startTime, totalDays);
+            var runningTime = (DateTime.Now - startTime);
+            builder.AppendFormat("Jestem uruchomiony od {0:D}, czyli przez {1}.", startTime, runningTime.Humanize());
             builder.AppendLine();
             builder.AppendLine($"Obsłużyłem w tym czasie {messageCounter} wiadomości.");
-            builder.AppendFormat("To daje średnio ~{0:0.0} wiadomości dziennie.", messageCounter / totalDays);
+            builder.AppendFormat("To daje średnio ~{0:0.0} wiadomości dziennie.", messageCounter / runningTime.TotalDays);
             builder.AppendLine();
             builder.AppendFormat("Rozmiar bazy danych: {0}.", ByteSize.FromBytes(Database.Instance.FileSize).Humanize("#.##"));
             builder.AppendLine();
