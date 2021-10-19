191bcb38e1543d8a5981abd964bbb90d499627dd
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2021-10-19 19:21:34 +0200

    Rather large timeout needed for Tasmota.

--- Relays/Tasmota.cs
+++ Relays/Tasmota.cs
@@ -7,30 +7,30 @@ namespace MieszkanieOswieceniaBot.Relays
     {
         public Tasmota(string hostname)
         {
-            this.hostname = hostname;
+            flurlClient = new FlurlClient($"http://{hostname}").WithTimeout(TimeSpan.FromSeconds(10));
         }
 
         public bool State
         {
             get
             {
-                return PowerStateToBool($"http://{hostname}/cm?cmnd=Power".GetJsonAsync().GetAwaiter().GetResult().POWER);
+                return PowerStateToBool(flurlClient.Request("cm?cmnd=Power").GetJsonAsync().GetAwaiter().GetResult().POWER);
             }
 
             set
             {
                 var commandValue = value ? "On" : "off";
-                $"http://{hostname}/cm?cmnd=Power%20{commandValue}".GetAsync().GetAwaiter().GetResult();
+                flurlClient.Request($"cm?cmnd=Power%20{commandValue}").GetAsync().GetAwaiter().GetResult();
             }
         }
 
         public bool Toggle()
         {
-            var jsonResult = $"http://{hostname}/cm?cmnd=Power%20Toggle".GetJsonAsync().GetAwaiter().GetResult();
+            var jsonResult = flurlClient.Request("/cm?cmnd=Power%20Toggle").GetJsonAsync().GetAwaiter().GetResult();
             return PowerStateToBool(jsonResult.POWER);
         }
 
-        private readonly string hostname;
+        private readonly IFlurlClient flurlClient;
 
         private static bool PowerStateToBool(string state)
         {
