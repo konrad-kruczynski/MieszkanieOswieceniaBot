297a944c26032b25ca504a47184c7a2df91cb07e
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2022-07-10 18:20:48 +0200

    Aquarium relay should rather be defunct.

---
+++ Source/Relays/DefunctRelay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MieszkanieOswieceniaBot.Relays
+{
+	public class DefunctRelay : IRelay
+	{
+		public DefunctRelay()
+		{
+		}
+
+        public Task<(bool Success, bool State)> TryGetStateAsync()
+        {
+            return Task.FromResult((true, false));
+        }
+
+        public Task<bool> TrySetStateAsync(bool state)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<(bool Success, bool CurrentState)> TryToggleAsync()
+        {
+            return Task.FromResult((false, false));
+        }
+    }
+}
+
