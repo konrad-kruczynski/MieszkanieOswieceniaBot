333329026538c03ddc321bb2af8ccf1df1bca2fc
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2023-04-02 08:26:21 +0200

    Localization based commands for heating pads.

--- Source/Commands/Heating.cs
+++ Source/Commands/Heating.cs
@@ -9,25 +9,25 @@ namespace MieszkanieOswieceniaBot.Commands
         public async Task<string> ExecuteAsync(TextCommandParameters parameters)
         {
             int relayNo;
-            if (!parameters.TryTakeString(out var whoToHeat))
+            if (!parameters.TryTakeString(out var whichToHeat))
             {
                 var relayNos = new[] { 4, 5 };
                 var relays = relayNos.Select(x => Globals.Relays[x].Relay);
                 var tasks = relays.Select(x => x.GetFriendlyStateAsync()).ToArray();
 
-                return string.Format("Kot: {0}{1}Kocica: {2}", await tasks[0], Environment.NewLine, await tasks[1]);
+                return string.Format("Prawa: {0}{1}Lewa: {2}", await tasks[0], Environment.NewLine, await tasks[1]);
             }
 
-            relayNo = whoToHeat switch
+            relayNo = whichToHeat switch
             {
-                "kot" => 4,
-                "kocica" => 5,
+                "prawa" => 4,
+                "lewa" => 5,
                 _ => -1
             };
 
             if (relayNo == -1)
             {
-                return "Niepoprawna informacja kogo grzać";
+                return "Niepoprawna informacja o tym, którą matę włączyć";
             }
             
             var (success, currentState) = await Globals.Relays[relayNo].Relay.TryToggleAsync();
